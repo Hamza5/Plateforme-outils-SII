f383529aa6b1d5ba823ee8a157bdc80affde62dd
Hamza5 <hamza.abbad@gmail.com>
2015-03-14 11:03:07 +0100

    Ajout automatique des onglets dans la fenêtre principale

---
+++ src/MainWindow.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class MainWindow extends JFrame {
+    private JTabbedPane tabs; // The main tabs
+    public MainWindow(){
+        super();
+        tabs = new JTabbedPane();
+        setPreferredSize(new Dimension(600, 400));
+        setContentPane(tabs);
+        // Add every component to the tabs automatically
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.endsWith(".class");
+            }
+        };
+        try {
+            URL pluginsURL = loader.getResource("Plugins");
+            if(pluginsURL != null){
+                File tabsDirectory = new File(pluginsURL.toURI());
+                File[] classFiles = tabsDirectory.listFiles(filter);
+                for (File c : classFiles){
+                    try {
+                        String className = tabsDirectory.getName() + '.' + c.getName().substring(0, c.getName().length() - 6); // Package.Class
+                        Class<?> tabClass = loader.loadClass(className);
+                        if(Component.class.isAssignableFrom(tabClass)){
+                            tabs.add((Component)tabClass.newInstance());
+                        }
+                    }
+                    catch (ClassNotFoundException | InstantiationException | IllegalAccessException | LinkageError e){
+                        JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            } else throw new FileNotFoundException("Can not find the Plugins package");
+        }
+        catch (FileNotFoundException | URISyntaxException e){
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+    public static void main(String[] args){ // Show the GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                mainWindow.pack();
+                mainWindow.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/Plugins/Tools.java
@@ -0,0 +1,55 @@
+package Plugins;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Tools extends JPanel {
+    private JTabbedPane tabs;
+    private JPanel ubcsatPage;
+    private DefaultListModel<String> modèle;
+    private JList<String> modèleList;
+    private AbstractAction ajouterFormuleAction;
+    private JPanel weightedmaxsatPage;
+    private final String title = "Tools";
+    private final int spacing = 5;
+    public Tools(){
+        super();
+        setName(title); // Will be the title of the tab
+        BoxLayout layout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
+        setLayout(layout);
+        tabs = new JTabbedPane();
+        add(tabs);
+        // Content of UBCSAT page
+        ubcsatPage = new JPanel();
+        ubcsatPage.setLayout(new BoxLayout(ubcsatPage, BoxLayout.PAGE_AXIS));
+        modèle = new DefaultListModel<>();
+        modèleList = new JList<>(modèle);
+        TitledBorder modèleListBorder = BorderFactory.createTitledBorder("Le modèle");
+        modèleList.setBorder(modèleListBorder);
+        modèleList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        final Component tools = this;
+        ajouterFormuleAction = new AbstractAction("Ajouter une formule") {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String formule = JOptionPane.showInputDialog(tools, "La formule", "Nouvelle formule", JOptionPane.QUESTION_MESSAGE);
+                if ( formule != null && !formule.isEmpty())
+                    modèle.addElement(formule);
+            }
+        };
+        JButton ajouterFormuleButton = new JButton(ajouterFormuleAction);
+        ubcsatPage.add(new JScrollPane(modèleList));
+        Box buttonsBox = new Box(BoxLayout.LINE_AXIS);
+        buttonsBox.setBorder(BorderFactory.createEmptyBorder(spacing,0,0,0));
+        buttonsBox.add(ajouterFormuleButton);
+        buttonsBox.add(Box.createHorizontalGlue());
+        ubcsatPage.add(buttonsBox);
+        ubcsatPage.setBorder(BorderFactory.createEmptyBorder(spacing, spacing, spacing, spacing));
+        // Content of Weighted Max SAT
+        weightedmaxsatPage = new JPanel();
+        // Add the pages
+        tabs.addTab("UBCSAT", ubcsatPage);
+        tabs.addTab("Weighted Max SAT", weightedmaxsatPage);
+    }
+}
