e23d2b47b62556cdd8781fb1a0620375a0b58726
Hamza5 <hamza.abbad@gmail.com>
2015-03-15 21:58:05 +0100

    Fonctionnement de base pour le calcule

--- src/Plugins/Tools.java
+++ src/Plugins/Tools.java
@@ -2,11 +2,17 @@ package Plugins;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 
 public class Tools extends JPanel {
@@ -16,6 +22,7 @@ public class Tools extends JPanel {
     private final JList<String> modèleList;
     private final AbstractAction ajouterFormuleAction;
     private final AbstractAction supprimerFormuleAction;
+    private final AbstractAction calculerAction;
     private final JPanel weightedmaxsatPage;
     private final String title = "Tools";
     private final int spacing = 5;
@@ -38,9 +45,9 @@ public class Tools extends JPanel {
         ajouterFormuleAction = new AbstractAction("Ajouter une formule") {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String formule = JOptionPane.showInputDialog(tools, "La formule (Utiliser '-' ou '!' pour le négation)", "Nouvelle formule", JOptionPane.QUESTION_MESSAGE);
+                String formule = JOptionPane.showInputDialog(tools, "La formule (Utiliser '-' pour le négation)", "Nouvelle formule", JOptionPane.QUESTION_MESSAGE);
                 if (formule != null && !formule.isEmpty())
-                    if (formule.matches("(([!-]?\\w+)\\s+)*([!-]?\\w+)"))
+                    if (formule.matches("(([-]?\\w+)\\s+)*([-]?\\w+)"))
                         modèle.addElement(formule.replaceAll("\\s+", " "));
                     else JOptionPane.showMessageDialog(tools, "La syntaxe de la formule est invalide !", "Formule invalide", JOptionPane.WARNING_MESSAGE);
             }
@@ -58,11 +65,77 @@ public class Tools extends JPanel {
         };
         supprimerFormuleAction.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_S);
         final JButton supprimerFormuleButton = new JButton(supprimerFormuleAction);
-        supprimerFormuleButton.setEnabled(false);
+        supprimerFormuleAction.setEnabled(false);
         modèleList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent listSelectionEvent) {
-                supprimerFormuleButton.setEnabled(modèleList.getSelectedIndices().length > 0);
+                supprimerFormuleAction.setEnabled(modèleList.getSelectedIndices().length > 0);
+            }
+        });
+        calculerAction = new AbstractAction("Calculer") {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    File tempFile = File.createTempFile("UBCSAT", ".txt");
+                    PrintWriter writer = new PrintWriter(tempFile);
+                    HashMap<String, Integer> clauses = new HashMap<>();
+                    int k = 1;
+                    String content = "";
+                    for (int i=0; i<modèle.size(); i++) {
+                        for (String c : modèle.get(i).split("\\s")) {
+                            String c_ = c.replace("-", "");
+                            if (!clauses.keySet().contains(c_))
+                                clauses.put(c_, k++);
+                            content += (c.charAt(0) == '-' ? "-" : "") + clauses.get(c_)+"\t";
+                        }
+                        content += "0\n";
+                    }
+                    content = "p\tcnf\t"+clauses.size()+'\t'+modèle.size()+'\n' + content;
+                    Runtime runtime = Runtime.getRuntime();
+                    writer.print(content);
+                    writer.close();
+                    URL pluginsURL = ClassLoader.getSystemClassLoader().getResource("Plugins");
+                    if (pluginsURL == null) throw new FileNotFoundException("Plugins package not found");
+                    String pluginsPath = pluginsURL.getPath();
+                    String[] cmd = {Paths.get(pluginsPath, "ubcsat").toString(), "-alg", "saps", "-i",
+                                    tempFile.getAbsolutePath(), "-solve", "-r", "out", "null", "-r", "stats", "null"};
+                    Process process = runtime.exec(cmd, null, new File(pluginsPath));
+                    InputStream output = process.getInputStream();
+                    InputStream errorsOutput = process.getErrorStream();
+                    int c;
+                    String results = "";
+                    while ((c = output.read()) != -1) {
+                        results += (char)c;
+                    }
+                    String errors = "";
+                    while ((c = errorsOutput.read()) != -1) {
+                        errors += (char)c;
+                    }
+                    if (!errors.isEmpty()) JOptionPane.showMessageDialog(tools, errors, "Erreur", JOptionPane.ERROR_MESSAGE);
+                    else JOptionPane.showMessageDialog(tools, results, "Résultats", JOptionPane.INFORMATION_MESSAGE);
+                }
+                catch (IOException e){
+                    JOptionPane.showMessageDialog(tools, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        };
+        calculerAction.setEnabled(false);
+        calculerAction.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_C);
+        final JButton calculerButton = new JButton(calculerAction);
+        modèle.addListDataListener(new ListDataListener() {
+            @Override
+            public void intervalAdded(ListDataEvent listDataEvent) {
+                calculerAction.setEnabled(true);
+            }
+
+            @Override
+            public void intervalRemoved(ListDataEvent listDataEvent) {
+                calculerAction.setEnabled(!modèle.isEmpty());
+            }
+
+            @Override
+            public void contentsChanged(ListDataEvent listDataEvent) {
+
             }
         });
         ubcsatPage.add(new JScrollPane(modèleList));
@@ -72,6 +145,7 @@ public class Tools extends JPanel {
         buttonsBox.add(Box.createRigidArea(new Dimension(spacing, 0)));
         buttonsBox.add(supprimerFormuleButton);
         buttonsBox.add(Box.createHorizontalGlue());
+        buttonsBox.add(calculerButton);
         ubcsatPage.add(buttonsBox);
         ubcsatPage.setBorder(BorderFactory.createEmptyBorder(spacing, spacing, spacing, spacing));
         // Content of Weighted Max SAT
